c58ac5bff809fa6ce246ac658c6f16ce34ed4a13
Thomas Davidson <tjd45@alumni.cam.ac.uk>
2022-02-21 16:01:30 +0100

    undoing refactoring back to hook version

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 //Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
@@ -9,84 +9,40 @@ import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
 import Actor from './Actor';
-//API
-import API from '../API';
+//Hook
+import { useMovieFetch } from '../hooks/useMovieFetch'
 //Image
 import NoImage from '../images/no_image.jpg';
 
-class Movie extends Component {
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId)
 
-    state = {
-        movie: {},
-        loading: true,
-        error: false
-    };
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
 
-    fetchMovie = async () => {
-        const { movieId } = this.props.params;
-        try {
-            this.setState({ error: false, loading: true });
-
-
-            const movie = await API.fetchMovie(movieId);
-            const credits = await API.fetchCredits(movieId);
-            //Get directors only
-            const directors = credits.crew.filter(
-                member => member.job === 'Director'
-            );
-
-            this.setState({
-                movie: {
-                    ...movie,
-                    actors: credits.cast,
-                    directors
-                },
-                loading: false
-            });
-
-
-        } catch (error) {
-            this.setState({ error: true, loading: false })
-        }
-    };
-
-    componentDidMount() {
-        this.fetchMovie();
-    }
-
-    render() {
-
-        const { movie, loading, error } = this.state;
-
-        if (loading) return <Spinner />;
-        if (error) return <div>Something went wrong...</div>;
-
-        return (
-            <>
-                <BreadCrumb movieTitle={movie.original_title} />
-                <MovieInfo movie={movie} />
-                <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
-                <Grid header='Actors'>
-                    {movie.actors.map(actor => (
-                        <Actor
-                            key={actor.credit_id}
-                            name={actor.name}
-                            character={actor.character}
-                            imageUrl={
-                                actor.profile_path
-                                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-                                    : NoImage
-                            }
-                        />
-                    ))}
-                </Grid>
-            </>
-        );
-
-    }
-
+    return (
+        <>
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
+        </>
+    );
 };
 
-const MovieWithParams = props => <Movie{...props} params={useParams()} />
 
-export default MovieWithParams;
+export default Movie;
