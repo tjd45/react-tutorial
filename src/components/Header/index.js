47391e5779565d956be59e961ccb3eafb7b7114c
Thomas Davidson <tjd45@alumni.cam.ac.uk>
2022-02-23 13:44:08 +0100

    bonus lessons etc

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+//Context
+import { Context } from '../../context';
+
 
 // Non arrow function syntax:
 // function Header() {};
 
-const Header = () => (
-    <Wrapper>
-        <Content>
-            <Link to='/'>
-                <LogoImg src={RMDBLogo} alt='rmdb-logo' />
-            </Link>
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
-        </Content>
-    </Wrapper>
-);
+const Header = () => {
+    const [user] = useContext(Context);
+
+    return (
+        <Wrapper>
+            <Content>
+                <Link to='/'>
+                    <LogoImg src={RMDBLogo} alt='rmdb-logo' />
+                </Link>
+                {user ? (
+                    <span className='loggedin'>Logged in as: {user.username}</span>
+                ) : (
+                        <Link to='/login'>
+                            <span className='login'>Log in</span>
+                        </Link>
+                    )
+
+
+                }
+                <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+            </Content>
+        </Wrapper>
+    );
+
+};
 
 export default Header;
